1f7ebee4206fce25d9aeaaf5584839a1730dd1ea
useonglee <useong0830@naver.com>
2021-12-06 19:50:03 +0900

    nextjs blog: Learn route query

--- nextjs-blog/pages/posts/first-post.jsx
+++ nextjs-blog/pages/posts/first-post.jsx
@@ -1,8 +1,20 @@
+import { useEffect } from "react";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import Layout from "../../components/layout";
 
 function FirstPost() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/posts/first-post/?counter=10", undefined, { shallow: true });
+  }, []);
+
+  useEffect(() => {
+    alert(router.query.counter);
+  }, [router.query.counter]);
+
   return (
     <Layout>
       <Head>
