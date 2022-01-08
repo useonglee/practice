e2a32424dc91e9c8edb482509bd93ede72c95512
useonglee <useong0830@naver.com>
2022-01-08 22:38:45 +0900

    practice testing code with testing-library/react

--- testing/react-test-app/src/App.js
+++ testing/react-test-app/src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled] = useState(false);
+
+  const handlePlusButton = () => {
+    setCounter((count) => count + 1);
+  };
+
+  const handleMinusButton = () => {
+    setCounter((count) => count - 1);
+  };
+
+  const handleDisabled = () => {
+    setDisabled((prev) => !prev);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testId="counter">{counter}</h3>
+        <div>
+          <button
+            onClick={handleMinusButton}
+            disabled={disabled}
+            data-testId="minus-button"
+          >
+            -
+          </button>
+          <button
+            onClick={handlePlusButton}
+            disabled={disabled}
+            data-testId="plus-button"
+          >
+            +
+          </button>
+        </div>
+        <div>
+          <button
+            data-testId="on/off-button"
+            style={{ backgroundColor: "blue" }}
+            onClick={handleDisabled}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
