fa18960244a5de0c921d9286e16676071628e28a
useonglee <useong0830@naver.com>
2022-03-29 23:58:29 +0900

    Add LongTitle to Task stories file

--- taskbox/src/components/Task.js
+++ taskbox/src/components/Task.js
@@ -22,7 +22,6 @@ export default function Task({
           value={title}
           readOnly={true}
           placeholder="Input title"
-          style={{ background: "red" }}
         />
       </div>
 
